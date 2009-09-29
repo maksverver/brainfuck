d537c397fe667f6802a6ad9651046a51a65468b4
Maks Verver <maksverver@geocities.com>
2009-09-29 22:14:28 +0200

    Increased built-in limits of interpreter (especially the code size limit needed to be increased). Added Perl script to remove non-Brainfuck characters from a file.

--- interpreter.c
+++ interpreter.c
@@ -2,64 +2,64 @@
 
 int main(int argc, char *argv[])
 {
-	FILE *fp;
-	char code[4096], *ip = code, *eof;
-	unsigned char data[4096] = { }, *dp = data;
-	int n;
- 
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s <program>\n", argv[0]);
-		return 1;
-	}
+    FILE *fp;
+    char code[65536], *ip = code, *eof;
+    unsigned char data[65536] = { }, *dp = data;
+    int n;
 
-	fp = fopen(argv[1], "rt");
-	if (!fp)
-	{
-		fprintf(stderr, "Could not open \"%s\"!\n");
-		return 1;
-	}
-	eof = code + fread(code, 1, sizeof(code), fp);
-	fclose(fp);
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <program>\n", argv[0]);
+        return 1;
+    }
 
-	while (ip != eof)
-	{
-		switch (*ip++)
-		{
-		case '+': ++*dp; break;
-		case '-': --*dp; break;
-		case '>': ++dp; break;
-		case '<': --dp; break;
-		case ',':
-			if ((n = getchar()) >= 0)
-				*dp = n;
-			break;
-		case '.':
-			putchar(*dp);
-			fflush(stdout);
-			break;
-		case '[':
-			if (!*dp)
-				for (n = 1; n; ++ip) 
-					n += (*ip == '[') - (*ip == ']');
-			break;
-		case ']': 
-			if (*dp)
-			{
-				ip -= 2;
-				for (n = 1; n; --ip) 
-					n += (*ip == ']') - (*ip == '[');
-				ip += 2;
-			}
-			break;
-		case '!':
-			printf("%04x  ", ip - code);
-			for (n = 0; n < 32; ++n)
-				printf("%c%02x", (dp == &data[n]) ? '>' : ' ' , data[n]);
-			printf("\n");
-			break;
-		}
-	}
+    fp = fopen(argv[1], "rt");
+    if (!fp)
+    {
+        fprintf(stderr, "Could not open \"%s\"!\n");
+        return 1;
+    }
+    eof = code + fread(code, 1, sizeof(code), fp);
+    fclose(fp);
 
-	return 0;
+    while (ip != eof)
+    {
+        switch (*ip++)
+        {
+        case '+': ++*dp; break;
+        case '-': --*dp; break;
+        case '>': ++dp; break;
+        case '<': --dp; break;
+        case ',':
+            if ((n = getchar()) >= 0)
+                *dp = n;
+            break;
+        case '.':
+            putchar(*dp);
+            fflush(stdout);
+            break;
+        case '[':
+            if (!*dp)
+                for (n = 1; n; ++ip) 
+                    n += (*ip == '[') - (*ip == ']');
+            break;
+        case ']': 
+            if (*dp)
+            {
+                ip -= 2;
+                for (n = 1; n; --ip) 
+                    n += (*ip == ']') - (*ip == '[');
+                ip += 2;
+            }
+            break;
+        case '!':
+            printf("%04x  ", ip - code);
+            for (n = 0; n < 32; ++n)
+                printf("%c%02x", (dp == &data[n]) ? '>' : ' ' , data[n]);
+            printf("\n");
+            break;
+        }
+    }
+
+    return 0;
 }
