384a193418f20251b8b37f639bd62281b10ffc88
Maks Verver <maksverver@geocities.com>
2010-05-21 17:44:16 +0200

    Updated Brainfuck to C compiler with simplifications and documentation.

--- bf2c.c
+++ bf2c.c
@@ -1,17 +1,28 @@
+/* A simple C program to generate C code from a Brainfuck program.
+
+  Uses a straightforward correspondence between Brainfuck operations and C
+  language constructs, except that consecutive identical operations are
+  collapsed to reduce the output size and required compilation time.
+
+  Brainfuck implementation properties:
+    - tape consists of 65536 characters (usually 8-bit bytes)
+    - tape head starts at the left end of the tape
+    - when reading fails (at end of file) the destination cell is unchanged
+
+  Maks Verver <maksverver@geocities.com> revised May 2010 */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
 
-int level = 0;
+static int indent = 0;
 
 void print(char *format, ...)
 {
+	int i = indent;
 	va_list ap;
-	int n;
 
-	for (n = 0; n < level; ++n)
-		printf("\t");
-	
+	while (i--) putchar('\t');
 	va_start(ap, format);
 	vprintf(format, ap);
 	va_end(ap);
@@ -20,74 +31,41 @@ void print(char *format, ...)
 
 int main()
 {
-	int c, d, move = 0;
+	int c, d, n;
+
 	print("#include <stdio.h>\n");
-	print("unsigned char data[65536], *p = data;\n");
-	print("void in() {");
-	++level;
+	print("static char t[65536], *p = t;\n");
+	print("static void in(void) {");
+	++indent;
 	print("int c = getchar();");
-	print("if (c != EOF) *p = (unsigned char)c;");
-	--level;
+	print("if (c != EOF) *p = (char)c;");
+	--indent;
 	print("}\n");
-	print("void out() {");
-	++level;
+	print("static void out(void) {");
+	++indent;
 	print("putchar(*p);");
 	print("fflush(stdout);");
-	--level;
+	--indent;
 	print("}\n");
 	print("int main() {");
-	++level;
-	while ((c = getchar()) != EOF)
+	++indent;
+	for (c = getchar(); c != EOF; c = d)
 	{
-		int n = 1;
-		for (;;) {
-			d = getchar();
-			if (c == d)
-			{
-				++n;
-				continue;
-			}
-			if (d == EOF || strchr("<>+-.,[]", d))
-			{
-				ungetc(d, stdin);
-				break;
-			}
-		}
-
-		if (strchr("<>",c))
-		{
-			move += c == '<' ? -n : +n;
-			if (!strchr("<>", d))
-			{
-				if (move > 0) print("p+=%d;", move);
-				if (move < 0) print("p-=%d;", -move);
-				move = 0;
-			}
-		}
-		else
-		if (c == '+' || c == '-')
-			print("*p%c=%d;", c, n);
-		else
-		if (c == '.' || c == ',')
-		{
-			while (n--)
-				print("%s();", c == '.' ? "out" : "in");
-		}
-		else
-		if (c == '[')
-		{
-			while (n--)
-				print("while(*p) {"), ++level;
-		}
-		else
-		if (c == ']')
-		{
-			while (n--)
-				--level, print("}");
+		n = 1;
+		while ((d = getchar()) == c) ++n;
+		switch (c) {
+		case '>': print("p+=%d;", n); break;
+		case '<': print("p-=%d;", n); break;
+		case '+': print("*p+=%d;", n); break;
+		case '-': print("*p-=%d;", n); break;
+		case ',': while (n--) print("in();"); break;
+		case '.': while (n--) print("out();"); break;
+		case '[': while (n--) print("while(*p) {"), ++indent; break;
+		case ']': while (n--) --indent, print("}"); break;
 		}
 	}
 	print("return 0;");
-	--level;
+	--indent;
 	print("}");
 	return 0;
 }
